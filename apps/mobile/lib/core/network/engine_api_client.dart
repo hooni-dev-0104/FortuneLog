f60eb828aba6efe99871b957646d05c8f6de1027
hooni.dev <hooni.dev@kakaomobility.com>
2026-02-14 07:43:19 +0900

    feat(mobile): add engine API client interface and DTO models

---
+++ apps/mobile/lib/core/network/engine_api_client.dart
@@ -0,0 +1,148 @@
+abstract interface class EngineApiClient {
+  Future<ChartResponseDto> calculateChart(CalculateChartRequestDto request);
+  Future<ReportResponseDto> generateReport(GenerateReportRequestDto request);
+  Future<DailyFortuneResponseDto> generateDailyFortune(
+    GenerateDailyFortuneRequestDto request,
+  );
+}
+
+class CalculateChartRequestDto {
+  final String userId;
+  final String birthDate;
+  final String birthTime;
+  final String birthTimezone;
+  final String birthLocation;
+  final String calendarType;
+  final bool leapMonth;
+  final String gender;
+  final bool unknownBirthTime;
+
+  const CalculateChartRequestDto({
+    required this.userId,
+    required this.birthDate,
+    required this.birthTime,
+    required this.birthTimezone,
+    required this.birthLocation,
+    required this.calendarType,
+    required this.leapMonth,
+    required this.gender,
+    required this.unknownBirthTime,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'userId': userId,
+    'birthDate': birthDate,
+    'birthTime': birthTime,
+    'birthTimezone': birthTimezone,
+    'birthLocation': birthLocation,
+    'calendarType': calendarType,
+    'leapMonth': leapMonth,
+    'gender': gender,
+    'unknownBirthTime': unknownBirthTime,
+  };
+}
+
+class GenerateReportRequestDto {
+  final String userId;
+  final String chartId;
+  final String reportType;
+
+  const GenerateReportRequestDto({
+    required this.userId,
+    required this.chartId,
+    required this.reportType,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'userId': userId,
+    'chartId': chartId,
+    'reportType': reportType,
+  };
+}
+
+class GenerateDailyFortuneRequestDto {
+  final String userId;
+  final String date;
+
+  const GenerateDailyFortuneRequestDto({
+    required this.userId,
+    required this.date,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'userId': userId,
+    'date': date,
+  };
+}
+
+class ChartResponseDto {
+  final String engineVersion;
+  final Map<String, String> chart;
+  final Map<String, int> fiveElements;
+
+  const ChartResponseDto({
+    required this.engineVersion,
+    required this.chart,
+    required this.fiveElements,
+  });
+
+  factory ChartResponseDto.fromJson(Map<String, dynamic> json) {
+    return ChartResponseDto(
+      engineVersion: json['engineVersion'] as String,
+      chart: (json['chart'] as Map<String, dynamic>).map(
+        (key, value) => MapEntry(key, value as String),
+      ),
+      fiveElements: (json['fiveElements'] as Map<String, dynamic>).map(
+        (key, value) => MapEntry(key, value as int),
+      ),
+    );
+  }
+}
+
+class ReportResponseDto {
+  final String chartId;
+  final String reportType;
+  final Map<String, dynamic> content;
+
+  const ReportResponseDto({
+    required this.chartId,
+    required this.reportType,
+    required this.content,
+  });
+
+  factory ReportResponseDto.fromJson(Map<String, dynamic> json) {
+    return ReportResponseDto(
+      chartId: json['chartId'] as String,
+      reportType: json['reportType'] as String,
+      content: json['content'] as Map<String, dynamic>,
+    );
+  }
+}
+
+class DailyFortuneResponseDto {
+  final String userId;
+  final String date;
+  final int score;
+  final Map<String, String> category;
+  final List<String> actions;
+
+  const DailyFortuneResponseDto({
+    required this.userId,
+    required this.date,
+    required this.score,
+    required this.category,
+    required this.actions,
+  });
+
+  factory DailyFortuneResponseDto.fromJson(Map<String, dynamic> json) {
+    return DailyFortuneResponseDto(
+      userId: json['userId'] as String,
+      date: json['date'] as String,
+      score: json['score'] as int,
+      category: (json['category'] as Map<String, dynamic>).map(
+        (key, value) => MapEntry(key, value as String),
+      ),
+      actions: List<String>.from(json['actions'] as List<dynamic>),
+    );
+  }
+}
